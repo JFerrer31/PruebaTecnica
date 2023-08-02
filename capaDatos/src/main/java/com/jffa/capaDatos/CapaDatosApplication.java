885b3d27ea7a2edc067128ce39e531441509c0e7
JFerrer31 <126852745+JFerrer31@users.noreply.github.com>
2023-08-01 22:52:03 -0600

    JFFA
    Se agrega proyecto para conectar a la BD

---
+++ capaDatos/src/main/java/com/jffa/capaDatos/CapaDatosApplication.java
@@ -0,0 +1,13 @@
+package com.jffa.capaDatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapaDatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapaDatosApplication.class, args);
+	}
+
+}
---
+++ capaDatos/src/test/java/com/jffa/capaDatos/CapaDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jffa.capaDatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapaDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
